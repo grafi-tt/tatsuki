cee7d2d04283fcd94c099d9b318deb5638de3d12
grafi <grafi@grafi.sakura.ne.jp>
2013-08-24 00:10:55 +0900

    add priority queue (not tested)

---
+++ PQueue.hs
@@ -0,0 +1,41 @@
+-- Leftist Heap
+module Data.PQueue.Max (MaxPQueue, union, empty, singleton, insert, findMax, deleteFindMax) where
+
+data MaxPQueue k v = Nil | Node Int k v (MaxPQueue k v) (MaxPQueue k v)
+
+-- public
+union :: Ord k => MaxPQueue k v -> MaxPQueue k v -> MaxPQueue k v
+union Nil u = u
+union t Nil = t
+union t@(Node _ kt vt lt rt) u@(Node _ ku vu lu ru)
+  | kt > ku   = leftistTree kt vt lt (union u rt)
+  | otherwise = leftistTree ku vu lu (union t ru)
+
+empty :: MaxPQueue k v
+empty = Nil
+
+singleton :: k -> v -> MaxPQueue k v
+singleton k v = Node 1 k v Nil Nil
+
+insert :: Ord k => k -> v -> MaxPQueue k v -> MaxPQueue k v
+insert k v = union $ singleton k v
+
+findMax :: Ord k => MaxPQueue k v -> (k, v)
+findMax = fst . deleteFindMax
+
+deleteMax :: Ord k => MaxPQueue k v -> MaxPQueue k v
+deleteMax = snd . deleteFindMax
+
+deleteFindMax :: Ord k => MaxPQueue k v -> ((k, v), MaxPQueue k v)
+deleteFindMax Nil = error "retriving value from empty MaxPQueue"
+deleteFindMax (Node _ k v l r) = ((k, v), union l r)
+
+-- private
+rank :: MaxPQueue k v -> Int
+rank Nil = 0
+rank (Node r _ _ _ _) = r
+
+leftistTree :: k -> v -> MaxPQueue k v -> MaxPQueue k v -> MaxPQueue k v
+leftistTree k v t u
+  | rank t > rank u = Node (rank u + 1) k v t u
+  | otherwise       = Node (rank t + 1 )k v u t
